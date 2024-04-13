35c4997e210070ad8e9dc594fe9c16d685efd135
Bencze Márton <benczemarton38@gmail.com>
2024-04-13 17:25:48 +0300

    alapfunkciokpipa

--- public/lab2/script.js
+++ public/lab2/script.js
@@ -1,3 +1,27 @@
+function szinez(figyeltText, beirtText) {
+  let figyelt = figyeltText.value.split(' ');
+  let beirt = beirtText.value;
+  let box = document.getElementById('cb');
+
+  for (let f in figyelt) {
+    let resz = figyelt[f];
+    let regex;
+    if (resz != ' ') {
+      if (box.checked) {
+        regex = new RegExp(resz, 'g');
+      } else {
+        regex = new RegExp(resz, 'gi');
+      }
+      beirt = beirt.replace(regex, `<span class="szin">${resz}</span>`);
+    }
+  }
+  document.getElementsByClassName('eredmeny')[0].innerHTML = beirt;
+}
+
 window.onload = () => {
-  console.log('Hello from JS');
+  let figyeltText = document.getElementById('figyeltszoveg');
+  let beirtText = document.getElementById('beirtszoveg');
+  beirtText.addEventListener('input', () => {
+    szinez(figyeltText, beirtText);
+  });
 };
