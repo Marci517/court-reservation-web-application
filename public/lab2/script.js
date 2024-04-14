3789f12f9decb260a7141c8dbf8b9de6e0807b08
Bencze Márton <benczemarton38@gmail.com>
2024-04-14 22:48:51 +0300

    keszlab2

--- public/lab2/script.js
+++ public/lab2/script.js
@@ -1,16 +1,16 @@
 function szinez(beirtText, figyeltText, box) {
-  let figyelt = figyeltText.value.trim().split(' ');
-  let beirt = beirtText.value.trim().split(' ');
-  let elem = document.getElementById('eredmeny');
+  const figyelt = figyeltText.value.trim().split(' ');
+  const beirt = beirtText.value.trim().split(' ');
+  const elem = document.getElementById('eredmeny');
 
   while (elem.firstChild) {
     elem.removeChild(elem.firstChild);
   }
 
   for (let i = 0; i < beirt.length; i++) {
-    let logtomb = [];
+    const logtomb = [];
     let szo = beirt[i];
-    let alap = szo;
+    const alap = szo;
     if (!box.checked) {
       szo = szo.toLowerCase();
     }
@@ -24,11 +24,11 @@ function szinez(beirtText, figyeltText, box) {
       }
       let startindex = szo.indexOf(figyeltszo);
       let ind = 0;
-      let startindexes = [];
+      const startindexes = [];
       while (startindex !== -1) {
         startindexes[ind] = startindex;
         startindex = szo.indexOf(figyeltszo, startindex + 1);
-        ind = ind + 1;
+        ind += 1;
       }
       for (let h = 0; h < startindexes.length; h++) {
         for (let k = 0; k < figyeltszo.length; k++) {
@@ -39,18 +39,18 @@ function szinez(beirtText, figyeltText, box) {
 
     for (let j = 0; j < szo.length; j++) {
       if (logtomb[j]) {
-        let span = document.createElement('span');
+        const span = document.createElement('span');
         span.textContent = alap[j];
         span.classList.add('piros');
         elem.appendChild(span);
       } else {
-        let span = document.createElement('span');
+        const span = document.createElement('span');
         span.textContent = alap[j];
         span.classList.add('fekete');
         elem.appendChild(span);
       }
     }
-    let span = document.createElement('span');
+    const span = document.createElement('span');
     span.textContent = ' ';
     span.classList.add('fekete');
     elem.appendChild(span);
