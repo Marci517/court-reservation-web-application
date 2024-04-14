14624b476262635df2e084309b3180bced1995ab
Bencze Márton <benczemarton38@gmail.com>
2024-04-14 22:44:57 +0300

    forokkal

--- public/lab2/script.js
+++ public/lab2/script.js
@@ -1,35 +1,76 @@
-function szinez(beirtText) {
-  const figyeltText = document.getElementById('figyeltszoveg');
-  let figyelt = figyeltText.value.trim();
-  figyelt = figyelt.split(' ');
-  let beirt = beirtText.value;
-  const box = document.getElementById('cb');
+function szinez(beirtText, figyeltText, box) {
+  let figyelt = figyeltText.value.trim().split(' ');
+  let beirt = beirtText.value.trim().split(' ');
+  let elem = document.getElementById('eredmeny');
 
-  for (let i = 0; i < figyelt.length; i++) {
-    const resz = figyelt[i];
-    let regex;
-    if (box.checked) {
-      regex = new RegExp(resz, 'g');
-    } else {
-      regex = new RegExp(resz, 'gi');
+  while (elem.firstChild) {
+    elem.removeChild(elem.firstChild);
+  }
+
+  for (let i = 0; i < beirt.length; i++) {
+    let logtomb = [];
+    let szo = beirt[i];
+    let alap = szo;
+    if (!box.checked) {
+      szo = szo.toLowerCase();
     }
-    beirt = beirt.replace(regex, `<span class="szin">${resz}</span>`);
+    for (let j = 0; j < szo.length; j++) {
+      logtomb[j] = false;
+    }
+    for (let j = 0; j < figyelt.length; j++) {
+      let figyeltszo = figyelt[j];
+      if (!box.checked) {
+        figyeltszo = figyeltszo.toLowerCase();
+      }
+      let startindex = szo.indexOf(figyeltszo);
+      let ind = 0;
+      let startindexes = [];
+      while (startindex !== -1) {
+        startindexes[ind] = startindex;
+        startindex = szo.indexOf(figyeltszo, startindex + 1);
+        ind = ind + 1;
+      }
+      for (let h = 0; h < startindexes.length; h++) {
+        for (let k = 0; k < figyeltszo.length; k++) {
+          logtomb[k + startindexes[h]] = true;
+        }
+      }
+    }
+
+    for (let j = 0; j < szo.length; j++) {
+      if (logtomb[j]) {
+        let span = document.createElement('span');
+        span.textContent = alap[j];
+        span.classList.add('piros');
+        elem.appendChild(span);
+      } else {
+        let span = document.createElement('span');
+        span.textContent = alap[j];
+        span.classList.add('fekete');
+        elem.appendChild(span);
+      }
+    }
+    let span = document.createElement('span');
+    span.textContent = ' ';
+    span.classList.add('fekete');
+    elem.appendChild(span);
   }
-  const elem = document.getElementById('eredmeny');
-  const gyerek = elem.childNodes;
-  for (let i = 0; i < gyerek.length; i++) {
-    elem.removeChild(gyerek[i]);
-  }
-  elem.appendChild(document.createTextNode(beirt));
 }
 
 window.onload = () => {
   const beirtText = document.getElementById('beirtszoveg');
+  const figyeltText = document.getElementById('figyeltszoveg');
+  const box = document.getElementById('cb');
+
   beirtText.addEventListener('input', () => {
-    szinez(beirtText);
+    szinez(beirtText, figyeltText, box);
   });
 
-  document.getElementById('cb').addEventListener('input', () => {
-    szinez(beirtText);
+  figyeltText.addEventListener('input', () => {
+    szinez(beirtText, figyeltText, box);
+  });
+
+  box.addEventListener('input', () => {
+    szinez(beirtText, figyeltText, box);
   });
 };
