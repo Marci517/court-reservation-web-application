b2fa8ebce21a220f85239396c0bda81cf9e7b820
Bencze Márton <benczemarton38@gmail.com>
2024-05-19 12:08:22 +0300

    kijavitvalab4

--- routes/bevezet.js
+++ routes/bevezet.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import Joi from 'joi';
+import { addPalya } from '../db/db.js';
 
 const router = express.Router();
 
@@ -8,6 +10,32 @@ router.get('/bevezet', (req, res) => {
     err: 0,
     errmess: 'Helytelen bemenet',
   });
+});
+
+router.post('/palyabevezet', express.urlencoded({ extended: true }), async (req, res) => {
+  console.log('bent a palyabevezetben');
+  const data = req.body;
+
+  const expected = Joi.object({
+    palyak: Joi.string().required(),
+    f0oraber: Joi.number().min(0).max(100000).required(),
+    f0cim: Joi.string().required(),
+    f0leiras: Joi.string().required(),
+  });
+
+  const { error } = expected.validate(data);
+  if (error != null) {
+    console.log('Helytelen bemenet a palyabevezetnel!');
+    res.render('bevezet', {
+      err: 1,
+      errmess: error.details[0].message,
+    });
+    return;
+  }
+
+  await addPalya(data.palyak, data.f0oraber, data.f0cim, data.f0leiras);
+  res.redirect('/');
+  console.log('Sikeres feltoltes');
 });
 
 export default router;
--- routes/index.js
+++ routes/index.js
@@ -1,35 +1,9 @@
 import express from 'express';
 import Joi from 'joi';
-import { addPalya, getPalyak, getPalyak2 } from '../db/db.js';
+import { getPalyak, getPalyak2 } from '../db/db.js';
 import { getminmaxnev } from '../utils/utils.js';
 
 const router = express.Router();
-
-router.post('/palyabevezet', express.urlencoded({ extended: true }), async (req, res) => {
-  console.log('bent a palyabevezetben');
-  const data = req.body;
-
-  const expected = Joi.object({
-    palyak: Joi.string().required(),
-    f0oraber: Joi.number().min(0).max(100000).required(),
-    f0cim: Joi.string().required(),
-    f0leiras: Joi.string().required(),
-  });
-
-  const { error } = expected.validate(data);
-  if (error != null) {
-    console.log('Helytelen bemenet a palyabevezetnel!');
-    res.render('bevezet', {
-      err: 1,
-      errmess: error.details[0].message,
-    });
-    return;
-  }
-
-  await addPalya(data.palyak, data.f0oraber, data.f0cim, data.f0leiras);
-  res.redirect('/');
-  console.log('Sikeres feltoltes');
-});
 
 router.get('/', async (req, res) => {
   console.log('bent a klienszurben');
